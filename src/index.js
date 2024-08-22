d82dee9857458bfdea3aa8fc1e8c2b5ade38acee
Rixels <mikey.drozd@gmail.com>
2024-08-22 12:25:52 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -35,11 +35,40 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' ',
 };
 
+
+
+
 function decode(expr) {
-    // write your solution here
+    let letter = [];
+    for ( let i = 0; i < expr.length; i = i + 10 ) {
+        letter = letter.concat([[expr.slice(i, (i + 2)), expr.slice((i + 2), (i + 4)), expr.slice((i + 4), (i + 6)), expr.slice((i + 6), (i + 8)), expr.slice((i + 8), (i + 10))]])
+    }
+    let connection = [];
+  letter.forEach((value, index) => {
+   let symbol = [];
+  for( let i = 0; i < 5; i = i + 1){
+  if (letter[index][i] === "10"){ symbol[i] = '.' }
+	else if (letter[index][i] === "11"){ symbol[i] = '-' } 
+  else if (letter[index][i] === "00"){ symbol[i] = '' }
+  else {symbol[i] = ' '}
+  
+  }
+ let unite = symbol.join('')
+ connection[index] = unite;
+  })
+  let str =''; 
+  connection.forEach((value, index) => {
+   str = str + MORSE_TABLE[connection[index]]
+  }
+  )
+  
+ return str;
 }
+
+
 
 module.exports = {
     decode
